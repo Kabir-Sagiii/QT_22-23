0d84789a17920fdf8419a8517250463ba71f5fd2
Kabir <sagithedeveloper@gmail.com>
2025-02-28 13:01:02 +0530

    Added the code

---
+++ 02_Javascript/S02/Task1.js
@@ -0,0 +1,8 @@
+var x = 10;
+
+x = 20;
+
+x = "sagar";
+
+//Loosely typed Programming
+//Strongly typed Programming
---
+++ 02_Javascript/S02/Variable.js
@@ -0,0 +1,8 @@
+var age;
+let pin;
+
+age = 18;
+pin = 12345;
+
+var name = "Raj Sharma";
+const password = 3456;
