3ec755146286a81eddecabf15af33fb0ab91ecb4
Kabir <sagithedeveloper@gmail.com>
2025-03-03 14:21:46 +0530

    Added the code

---
+++ 02_Javascript/S03/Function.js
@@ -0,0 +1,12 @@
+function fnSum() {
+  var x = 90;
+  var y = 20;
+
+  var result = x + y;
+
+  console.log(x, y, result);
+}
+
+fnSum();
+fnSum();
+fnSum();
---
+++ 02_Javascript/S03/Sum.js
@@ -0,0 +1,31 @@
+var x = 10;
+var y = 20;
+
+var result = x + y;
+
+console.log(x, y, result);
+
+var x = 30;
+var y = 20;
+
+var result = x + y;
+
+console.log(x, y, result);
+var x = 50;
+var y = 20;
+
+var result = x + y;
+
+console.log(x, y, result);
+var x = 70;
+var y = 20;
+
+var result = x + y;
+
+console.log(x, y, result);
+var x = 90;
+var y = 20;
+
+var result = x + y;
+
+console.log(x, y, result);
